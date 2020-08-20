68743b8a932151756d51ebea75a527e47f3cfbb9
sanju-developer <vivekrajoriya106@gmail.com>
2020-08-20 23:15:17 +0530

    change api integration

--- src/components/githubListing/githubListBody.jsx
+++ src/components/githubListing/githubListBody.jsx
@@ -2,22 +2,8 @@ import React from "react";
 
 export const GithubListBody = ({ repo }) => {
   return (
-    <div className="repo-list">
+    <div className="repo-item">
       <div className="repo-left border2px">
-        <div className="repo-inner-left">
-          <div className="repo-info border2px">{repo.name}</div>
-          <div className="repo-info border2px">{repo.url}</div>
-          <div className="repo-info border2px">{repo.languages_url}</div>
-        </div>
-        <div className="repo-inner-right">
-          <img
-            className="delete-icon"
-            src="https://cdn.iconscout.com/icon/free/png-512/delete-737-475058.png"
-            alt="delete-icon"
-          />
-        </div>
-      </div>
-      <div className="repo-right border2px">
         <div className="repo-inner-left">
           <div className="repo-info border2px">{repo.name}</div>
           <div className="repo-info border2px">{repo.url}</div>
--- src/components/githubListing/index.jsx
+++ src/components/githubListing/index.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { GithubListBody } from './githubListBody'
-import { GithubListTopBar } from './githubListTopBar'
+import { GithubListBody } from "./githubListBody";
+import { GithubListTopBar } from "./githubListTopBar";
 
 import "./githubListing.css";
 
 export const GithubListing = () => {
-  const [repoList, setRepoList] = useState([]);
+  const [repoList, setRepoList] = useState([])
 
-// fetch data from server on component mount
+  // fetch data from server on component mount
   useEffect(() => {
     fetchGithubList();
   }, []);
@@ -22,13 +22,21 @@ export const GithubListing = () => {
 
   return (
     <div className="githubListing-container">
-    {repoList.map(repo => {
-      return <div className="sections">
-      <GithubListTopBar repo={repo}/>
-      <h5 className="text-center">All repositories of {repo.owner.login}</h5>
-      <GithubListBody repo={repo}/>
-      </div>
-    })}
+      {repoList.map((repo, index) => {
+        return (
+          <>
+            {index === 0 && (
+              <>
+                <GithubListTopBar repo={repo} />
+                <h5 className="text-center">
+                  All repositories of {repo.owner.login}
+                </h5>
+              </>
+            )}
+              <GithubListBody repo={repo} />
+          </>
+        );
+      })}
     </div>
   );
 };
